9229c59e88837aa2409e364d55152b2d0b876976
Jugep001 <juliang1@live.de>
2021-01-14 16:02:24 +0100

    - server.py nach Beims Vorgaben geändert

--- app/template.py
+++ app/template.py
@@ -35,7 +35,6 @@ class Template_cl(object):
         # -------------------------------------------------------
         self.currDir = currDir
 
-
     # -------------------------------------------------------
     def GET(self):
         # -------------------------------------------------------
@@ -44,12 +43,14 @@ class Template_cl(object):
         }
 
         files_a = os.listdir(os.path.join(self.currDir, 'templates'))
+
         for fileName_s in files_a:
             file_o = codecs.open(os.path.join(self.currDir, 'templates', fileName_s), 'rU',
                                  'utf-8')
             content_s = file_o.read()
             file_o.close()
             retVal_o["templates"][fileName_s] = content_s
+
 
         return json.dumps(retVal_o)
 
--- server.py
+++ server.py
@@ -1,66 +1,101 @@
-# coding: utf-8
+# coding:utf-8
+
+# Demonstrator es/te/tm
 
 import sys
-import os
+import os.path
 import cherrypy
-from app import application
+from app import Mitarbeiter
+
+from app import application, template
 
 
-class Generator_cl():
-    def __init__(self, currDir):
-        self.app = application.Application_cl(currDir)
+# ----------------------------------------------------------
+def main(currDir):
+    # ----------------------------------------------------------
+
+    # aktuelles Verzeichnis ermitteln, damit es in der Konfigurationsdatei als
+    # Bezugspunkt verwendet werden kann
+    try:  # aktuelles Verzeichnis als absoluter Pfad
+        currentDir_s = os.path.dirname(os.path.abspath(__file__))
+    except:
+        currentDir_s = os.path.dirname(os.path.abspath(sys.executable))
+    cherrypy.Application.currentDir_s = currentDir_s
+
+    configFileName_s = os.path.join(currentDir_s, 'server.conf')  # im aktuellen Verzeichnis
+    if os.path.exists(configFileName_s) == False:
+        # Datei gibt es nicht
+        configFileName_s = None
+
+    # autoreload-Monitor hier abschalten
+    cherrypy.engine.autoreload.unsubscribe()
+
+    # 1. Eintrag: Standardverhalten, Berücksichtigung der Konfigurationsangaben im configFile
+    cherrypy.tree.mount(
+        None, '/', configFileName_s
+    )
+
+    # 2. Eintrag: Method-Dispatcher für die "Applikation" "app" vereinbaren
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Mitarbeiter',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Weiterbildung',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Mitarbeiteruebersicht',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Weiterbildungsuebersicht',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Mitarbeiterqualifikation',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Auswertungmitarbeiter',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Auswertungweiterbildungen',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.tree.mount(
+        Mitarbeiter.Mitarbeiter_cl(currDir),
+        '/Auswertungzertifikate',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    # 2. Eintrag: Method-Dispatcher für die "Applikation" "templates" vereinbaren
+    cherrypy.tree.mount(
+        template.Template_cl(currDir),
+        '/templates',
+        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    )
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
 
 
+# ----------------------------------------------------------
 if __name__ == '__main__':
-    conf = {
-        '/': {
-            'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd()),
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': './content',
-            'tools.staticdir.index': 'index.html'
-        },
-        '/Mitarbeiter': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Weiterbildung': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Mitarbeiteruebersicht': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Weiterbildunguebersicht': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Mitarbeiterqualifikation': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Auswertungmitarbeiter': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Auswertungweiterbildungen': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/Auswertungzertifikate': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-        '/template': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-        },
-    }
-    # Mount static content handler
-    currDir = os.path.abspath(os.getcwd())  # Aktuellen Dateipfad ziehen
-    webapp = Generator_cl(currDir)  # Erstellen der Webapp
-
-    # Zuweisen der Dispatcherklassen
-    webapp.template = webapp.app.template_obj
-    webapp.Auswertungmitarbeiter = webapp.app.Auswertungmitarbeiter_obj
-    webapp.Auswertungweiterbildungen = webapp.app.Auswertungweiterbildungen_obj
-    webapp.Auswertungzertifikate = webapp.app.Auswertungzertifikate_obj
-    webapp.Mitarbeiter = webapp.app.Mitarbeiter_obj
-    webapp.Weiterbildung = webapp.app.Weiterbildung_obj
-    webapp.Mitarbeiterqualifikation = webapp.app.Mitarbeiterqualifikation_obj
-    webapp.Mitarbeiteruebersicht = webapp.app.Mitarbeiteruebersicht_obj
-    webapp.Weiterbildunguebersicht = webapp.app.Weiterbildunguebersicht_obj
-
-    cherrypy.quickstart(webapp, '/', conf)  # Server starten
+    # ----------------------------------------------------------
+    main(os.path.abspath(os.getcwd()))
